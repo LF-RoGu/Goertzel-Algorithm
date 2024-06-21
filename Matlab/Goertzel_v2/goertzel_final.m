d1d5aad4e3471f3c876338e0fa14d4673820ea1b
Luis Fernando <36269176+LF-RoGu@users.noreply.github.com>
2024-06-21 11:10:18 +0200

    Update goertzel_final.m

--- Matlab/Goertzel_v2/goertzel_final.m
+++ Matlab/Goertzel_v2/goertzel_final.m
@@ -2,7 +2,7 @@ function magnitude = goertzel_final(state, coeff, scalingFactor, omega)
     cosine = cos(omega);
     sine = sin(omega);
     % Calculate the real and imaginary parts
-    real_part = (state(1) - state(2) * cosine) / scalingFactor;
-    imag_part = (state(2) * sine) / scalingFactor;
+    real_part = ((state(1) * cosine) - state(2)) / scalingFactor;
+    imag_part = (state(1) * sine) / scalingFactor;
     magnitude = sqrt(real_part^2 + imag_part^2);
 end
